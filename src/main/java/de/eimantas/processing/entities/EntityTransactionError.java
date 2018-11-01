397748e949c2df2412eecab7fd24fad281b303cb
eimyss <eimyss@gmail.com>
2018-11-01 15:04:56 +0100

    redesign messaging and added error logs

--- src/main/java/de/eimantas/processing/entities/EntityTransactionError.java
+++ src/main/java/de/eimantas/processing/entities/EntityTransactionError.java
@@ -4,9 +4,7 @@ package de.eimantas.processing.entities;
 import de.eimantas.processing.entities.types.EntityTransactionType;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.Instant;
 
 @Entity
@@ -17,7 +15,11 @@ public class EntityTransactionError {
   @GeneratedValue
   private long id;
   private EntityTransactionType type;
+  @Lob
+  @Column
   private String message;
+  @Lob
+  @Column
   private String exception;
   private Instant date;
 }
